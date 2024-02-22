ad220b1f7fb17d6979ee459c372f350393cbc572
nour-it <reply.nour.it@gmail.com>
2024-02-22 06:46:30 +0000

    structure de la base de données

---
+++ database/migrations/2024_02_22_061331_create_skills_table.php
@@ -0,0 +1,51 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('images', function (Blueprint $table) {
+            $table->id();
+            $table->string("path");
+        });
+        Schema::create('skill_categories', function (Blueprint $table) {
+            $table->id();
+            $table->string("name");
+            $table->dateTime("add_at");
+            $table->dateTime("delete_at")->nullable();
+            $table->string("description")->nullable();
+        });
+        Schema::create('skills', function (Blueprint $table) {
+            $table->id();
+            $table->string("name");
+            $table->dateTime("add_at");
+            $table->dateTime("delete_at")->nullable();
+            $table->string("description")->nullable();
+            $table->foreignid("skill_category_id")->nullable()->references("id")->on("skill_categories");
+        });
+        Schema::create('image_skill', function (Blueprint $table) {
+            $table->foreignid("image_id")->nullable()->references("id")->on("images");
+            $table->foreignid("skill_id")->nullable()->references("id")->on("skills");
+            $table->dateTime("upload_at");
+            $table->dateTime("delete_at")->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('image_skill');
+        Schema::dropIfExists('skills');
+        Schema::dropIfExists('categories');
+        Schema::dropIfExists('images');
+    }
+};
---
+++ database/migrations/2024_02_22_063607_create_projects_table.php
@@ -0,0 +1,53 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('project_categories', function (Blueprint $table) {
+            $table->id();
+            $table->string("name");
+            $table->dateTime("add_at");
+            $table->dateTime("delete_at")->nullable();
+            $table->string("description")->nullable();
+        });
+        Schema::create('projects', function (Blueprint $table) {
+            $table->id();
+            $table->string("name");
+            $table->dateTime("add_at");
+            $table->dateTime("delete_at")->nullable();
+            $table->string("description")->nullable();
+            $table->foreignid("project_category_id")->nullable()->references("id")->on("project_categories");
+        });
+        Schema::create('project_image', function (Blueprint $table) {
+            $table->foreignid("image_id")->nullable()->references("id")->on("images");
+            $table->foreignid("project_id")->nullable()->references("id")->on("projects");
+            $table->dateTime("upload_at");
+            $table->dateTime("delete_at")->nullable();
+        });
+        Schema::create('project_skill', function (Blueprint $table) {
+            $table->foreignid("skill_id")->nullable()->references("id")->on("skills");
+            $table->foreignid("project_id")->nullable()->references("id")->on("projects");
+            $table->dateTime("upload_at");
+            $table->dateTime("delete_at")->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('project_image');
+        Schema::dropIfExists('project_skill');
+        Schema::dropIfExists('projects');
+        Schema::dropIfExists('project_categories');
+    }
+};
